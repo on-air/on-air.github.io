e6361b3df2dc336c88e72fa10d8b05854bca693a
Seindi Rahmat Barus <seindi@outlook.com>
2022-05-20 16:32:19 +0700

    .

--- shell/npc.sh
+++ shell/npc.sh
@@ -2,9 +2,11 @@
 
 if [ "$1" == "--help" ]
 	then
-		echo "node package c --help"
+		echo "virtual package --help"
 elif [ "$1" == "init" ]
 	then
+		mkdir app
+		mkdir application
 		mkdir /var/node
 		mkdir /var/node/node_modules
 		mkdir /var/node/node_packages
@@ -17,17 +19,32 @@ elif [ "$1" == "update" ] && [ "$2" == "script" ]
 		sudo cp /tmp/npc.sh /usr/bin/npc
 elif [ "$1" == "update" ] && [ "$2" == "repository" ]
 	then
-		if [ "$3" == "" ]
+		if [ "$3" != "" ]
 			then
-				echo "unzip --password"
+				rm /tmp/node.zip
+				rm /tmp/vue.zip
+				wget -P /tmp/ https://cd.netizen.ninja/shell/node.zip
+				wget -P /tmp/ https://cd.netizen.ninja/shell/vue.zip
+				unzip -P $3 /tmp/node.zip -d /var/node/node_modules
+				unzip -P $3 /tmp/vue.zip -d /var/node/node_packages
 		else
-			rm /tmp/node.zip
-			wget -P /tmp/ https://cd.netizen.ninja/shell/node.zip
-			unzip -P $3 /tmp/node.zip -d /var/node/node_modules
+			echo "unzip --password"
 		fi
 elif [ "$1" == "update" ]
 	then
 		cp -r /var/node/node_modules/* node_modules/
+elif [ "$1" == "vue" ] && [ "$2" == "init" ]
+	then
+		if [ "$3" != "" ]
+			then
+				mkdir app/$3
+				cp -r /var/node/node_packages/cgi-bin app/$3/cgi-bin
+				cp -r /var/node/node_packages/cgi-public app/$3/cgi-public
+				mv app/$3/cgi-public/default app/$3/cgi-public/www
+				cd app/$3/cgi-public/www
+		else
+			echo "app --name"
+		fi
 else
 	echo ""
 	fi
