ec832dda8ae8e7f4b5ce003e112fd2de04ffeea4
Seindi Rahmat Barus <seindi@outlook.com>
2022-05-20 15:07:26 +0700

    .

--- shell/npc.sh
+++ shell/npc.sh
@@ -17,9 +17,14 @@ elif [ "$1" == "update" ] && [ "$2" == "script" ]
 		sudo cp /tmp/npc.sh /usr/bin/npc
 elif [ "$1" == "update" ] && [ "$2" == "repository" ]
 	then
-		rm /tmp/node.zip
-		wget -P /tmp/ https://cd.netizen.ninja/shell/node.zip
-		unzip -P $3 /tmp/node.zip -d /var/node/node_modules
+		if [ "$3" == "" ]
+			then
+				echo "unzip --password"
+		else
+			rm /tmp/node.zip
+			wget -P /tmp/ https://cd.netizen.ninja/shell/node.zip
+			unzip -P $3 /tmp/node.zip -d /var/node/node_modules
+		fi
 elif [ "$1" == "update" ]
 	then
 		cp -r /var/node/node_modules/* node_modules/
