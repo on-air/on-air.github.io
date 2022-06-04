9fa5e17f01efa0be4207a904904abf8db488376f
Seindi Rahmat Barus <seindi@outlook.com>
2022-06-04 15:42:21 +0700

    .

--- bot/install.sh
+++ bot/install.sh
@@ -9,15 +9,27 @@ bot_update () {
 		fi
 	rm /tmp/cli.sh
 	rm /tmp/cli.js
-	rm /tmp/bot.rar
+	rm /tmp/cli.min.js
+	rm /tmp/archive.rar
+	rm /tmp/ng.config
+	rm /tmp/my-sql.config
+	rm /tmp/firewall.rule
 	wget -P /tmp/ $bot_url/bot/cli.sh
 	wget -P /tmp/ $bot_url/bot/cli.js
-	wget -P /tmp/ $bot_url/bot/bot.rar
+	wget -P /tmp/ $bot_url/bot/cli.min.js
+	wget -P /tmp/ $bot_url/bot/archive.rar
+	wget -P /tmp/ $bot_url/bot/ng.config
+	wget -P /tmp/ $bot_url/db/my-sql.config
+	wget -P /tmp/ $bot_url/security/firewall.rule
 	sudo chmod +x /tmp/cli.sh
 	sudo cp /tmp/cli.sh /usr/bin/bot
 	cp /tmp/cli.js /var/bot/cli.js
+	cp /tmp/cli.min.js /var/bot/cli.min.js
 	cp /var/1.json /var/bot/cli.json
-	unrar x -P$1 /tmp/bot.rar /var/bot -o+
+	unrar x -P$1 /tmp/archive.rar /var/bot -o+
+	cp /tmp/ng.config /var/bot/ng.config
+	cp /tmp/my-sql.config /var/bot/my-sql.config
+	cp /tmp/firewall.rule /var/bot/firewall.rule
 	}
 
 bot_ng () {
@@ -39,14 +51,12 @@ bot_ng_reload () {
 bot_ng_setup () {
 	rm /etc/nginx/sites-enabled/default
 	rm /etc/nginx/sites-enabled/0.0.0.0
-	rm /tmp/ng.config
-	wget -P /tmp/ $bot_url/bot/ng.config
 	if [ -f "/etc/nginx/nginx.conf.bak" ]
 	then
-		cp /tmp/ng.config /etc/nginx/nginx.conf
+		cp /var/bot/ng.config /etc/nginx/nginx.conf
 	else
 		mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
-		cp /tmp/ng.config /etc/nginx/nginx.conf
+		cp /var/bot/ng.config /etc/nginx/nginx.conf
 		fi
 	}
 
@@ -55,8 +65,13 @@ bot_db_my_sql () {
 	}
 
 bot_db_my_sql_setup () {
-	mv /etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf.bak
-	cp /tmp/my-sql.config /etc/mysql/mysql.conf.d/mysqld.cnf
+	if [ ! -d "/etc/mysql/mysql.conf.d/mysqld.cnf.bak" ]
+	then
+		cp /var/bot/my-sql.config /etc/mysql/mysql.conf.d/mysqld.cnf
+	else
+		mv /etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf.bak
+		cp /var/bot/my-sql.config /etc/mysql/mysql.conf.d/mysqld.cnf
+		fi
 	}
 
 bot_db_my_sql_firewall_setup () {
@@ -81,14 +96,12 @@ bot_firewall_reload () {
 	}
 
 bot_firewall_rule_setup () {
-	rm /tmp/firewall.rule
-	wget -P /tmp/ https://cd.netizen.ninja/security/firewall.rule
 	if [ -f "/etc/ufw/before.rules.bak" ]
 	then
-		cp /tmp/firewall.rule /etc/ufw/before.rules
+		cp /var/bot/firewall.rule /etc/ufw/before.rules
 	else
 		mv /etc/ufw/before.rules /etc/ufw/before.rules.bak
-		cp /tmp/firewall.rule /etc/ufw/before.rules
+		cp /var/bot/firewall.rule /etc/ufw/before.rules
 		fi
 	}
 
