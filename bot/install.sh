e41f5642f92d16bbc929d82edd2ccd59f636d21b
Seindi Rahmat Barus <seindi@outlook.com>
2022-06-05 11:50:46 +0700

    .

--- bot/install.sh
+++ bot/install.sh
@@ -5,31 +5,57 @@ bot_url="https://cd.netizen.ninja"
 bot_update () {
 	if [ ! -d "/var/bot" ]
 	then
-		mkdir /var/bot
+		bot_update_init
 		fi
-	rm /tmp/cli.sh
-	rm /tmp/cli.js
-	rm /tmp/cli.min.js
-	rm /tmp/archive.rar
-	rm /tmp/ng.config
-	rm /tmp/my-sql.config
-	rm /tmp/firewall.rule
-	wget -P /tmp/ $bot_url/bot/cli.sh
-	wget -P /tmp/ $bot_url/bot/cli.js
-	wget -P /tmp/ $bot_url/bot/cli.min.js
-	wget -P /tmp/ $bot_url/bot/archive.rar
-	wget -P /tmp/ $bot_url/bot/ng.config
-	wget -P /tmp/ $bot_url/db/my-sql.config
-	wget -P /tmp/ $bot_url/security/firewall.rule
+	bot_update_clear
+	bot_update_download
 	sudo chmod +x /tmp/cli.sh
 	sudo cp /tmp/cli.sh /usr/bin/bot
+	cp /tmp/cli.sh /var/bot/cli.sh
 	cp /tmp/cli.js /var/bot/cli.js
 	cp /tmp/cli.min.js /var/bot/cli.min.js
 	cp /var/1.json /var/bot/cli.json
-	unrar x -P$1 /tmp/archive.rar /var/bot -o+
-	cp /tmp/ng.config /var/bot/ng.config
-	cp /tmp/my-sql.config /var/bot/my-sql.config
-	cp /tmp/firewall.rule /var/bot/firewall.rule
+	unrar x -P$1 /tmp/node.rar /var/bot/node_modules -o+
+	unrar x -P$1 /tmp/express.rar /var/bot/node_packages/express -o+
+	cp /tmp/my-sql.config /var/bot/node_process/my-sql.config
+	cp /tmp/firewall.rule /var/bot/node_process/firewall.rule
+	}
+
+bot_update_init () {
+	mkdir /var/bot
+	mkdir /var/bot/node_modules
+	mkdir /var/bot/node_packages
+	mkdir /var/bot/node_packages/express
+	mkdir /var/bot/node_process
+	}
+
+bot_update_clear () {
+	rm -rf /var/bot/node_modules/*
+	rm -rf /var/bot/node_packages/express/*
+	rm -rf /var/bot/node_process/*
+	rm /tmp/cli.sh
+	rm /tmp/cli.js
+	rm /tmp/cli.min.js
+	rm /tmp/node.rar
+	rm /tmp/express.rar
+	rm /tmp/apache.config
+	rm /tmp/ng.config
+	rm /tmp/ng.template
+	rm /tmp/my-sql.config
+	rm /tmp/firewall.rule
+	}
+
+bot_update_download () {
+	wget -P /tmp/ $bot_url/bot/cli.sh
+	wget -P /tmp/ $bot_url/bot/cli.js
+	wget -P /tmp/ $bot_url/bot/cli.min.js
+	wget -P /tmp/ $bot_url/bot/node_process/node.rar
+	wget -P /tmp/ $bot_url/bot/node_process/express.rar
+	wget -P /tmp/ $bot_url/bot/node_process/apache.config
+	wget -P /tmp/ $bot_url/bot/node_process/ng.config
+	wget -P /tmp/ $bot_url/bot/node_process/ng.template
+	wget -P /tmp/ $bot_url/db/my-sql.config
+	wget -P /tmp/ $bot_url/security/firewall.rule
 	}
 
 bot_ng () {
@@ -53,10 +79,10 @@ bot_ng_setup () {
 	rm /etc/nginx/sites-enabled/0.0.0.0
 	if [ -f "/etc/nginx/nginx.conf.bak" ]
 	then
-		cp /var/bot/ng.config /etc/nginx/nginx.conf
+		cp /var/bot/node_process/ng.config /etc/nginx/nginx.conf
 	else
 		mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
-		cp /var/bot/ng.config /etc/nginx/nginx.conf
+		cp /var/bot/node_process/ng.config /etc/nginx/nginx.conf
 		fi
 	}
 
@@ -67,10 +93,10 @@ bot_db_my_sql () {
 bot_db_my_sql_setup () {
 	if [ ! -d "/etc/mysql/mysql.conf.d/mysqld.cnf.bak" ]
 	then
-		cp /var/bot/my-sql.config /etc/mysql/mysql.conf.d/mysqld.cnf
+		cp /var/bot/node_process/my-sql.config /etc/mysql/mysql.conf.d/mysqld.cnf
 	else
 		mv /etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf.bak
-		cp /var/bot/my-sql.config /etc/mysql/mysql.conf.d/mysqld.cnf
+		cp /var/bot/node_process/my-sql.config /etc/mysql/mysql.conf.d/mysqld.cnf
 		fi
 	}
 
@@ -98,10 +124,10 @@ bot_firewall_reload () {
 bot_firewall_rule_setup () {
 	if [ -f "/etc/ufw/before.rules.bak" ]
 	then
-		cp /var/bot/firewall.rule /etc/ufw/before.rules
+		cp /var/bot/node_process/firewall.rule /etc/ufw/before.rules
 	else
 		mv /etc/ufw/before.rules /etc/ufw/before.rules.bak
-		cp /var/bot/firewall.rule /etc/ufw/before.rules
+		cp /var/bot/node_process/firewall.rule /etc/ufw/before.rules
 		fi
 	}
 
